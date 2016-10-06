1a0f1ba131bdbd3b42ab859523351faf65a6a0e4
Martin Hnátek <m.hnatma@gmail.com>
2016-10-06 17:50:50 +0200

    Přidána funkce, která vrací handler k databázi. Tato funkce počítá s existencí konfiguračního souboru config.ini ve kterém se budou nacházet přihlašovací údaje.

--- php/dbHandler.php
+++ php/dbHandler.php
@@ -1,14 +1,40 @@
 <?php
-$mysqli = new mysqli("localhost", "user", "password", "database");
-if ($mysqli->connect_errno) {
-    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+/**
+ * Načte konfigurační soubor, který musí obsahovat následující atributy.
+ * host - adresa databáze
+ * user - uživatelské jméno
+ * password - heslo
+ */
+function loadDatabaseCredentials()
+{
+	$data = parse_ini("config.ini");
+	if($data == False)
+	{
+		throw new Exception("Konfigurační soubor nebyl nalezen");
+	}
+	return $data;
 }
-echo $mysqli->host_info . "\n";
 
-$mysqli = new mysqli("127.0.0.1", "user", "password", "database", 3306);
-if ($mysqli->connect_errno) {
-    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+/**
+ * Vrátí handler pro databázi.
+ */
+function getDatabase()
+{
+	$credentials = loadDatabaseCredentials();
+	assert(isset($credentials["host"]), "Host se nenachází v konfiguračním souboru!");
+	assert(isset($credentials["user"]), "User se nenachází v konfiguračním souboru!");
+	assert(isset($credentials["password"]), "Password se nenachází v konfiguračním souboru!");
+	$host = $credentials["host"];
+	$user = $credentials["user"];
+	$password = $credentials["password"];
+	$charset = "utf8";
+	$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+	$opt = [
+		PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+		PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+	];
+	$handler = new PDO($dsn, $user, $pass, $opt);
+	$handler->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	return $handler;
 }
-
-echo $mysqli->host_info . "\n";
 ?>
